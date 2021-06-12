3f4bf993b6fb792f2bf285c6f440e486ec6028c9
Oluwaferanmi <54285588+Oluwaferanmi59@users.noreply.github.com>
2021-06-12 02:38:46 +0100

    Add files via upload

---
+++ madlibs.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string color, pluralNoun, celebrity;
+	cout<< "Enter a color: ";
+	getline(cin, color);
+	cout<< "Enter a pluralNoun: ";
+	getline(cin, pluralNoun);
+	cout<< "Enter a celebrity: ";
+	getline(cin, celebrity);
+	
+	cout<< "Roses are "<< color << "\n";
+	cout<< pluralNoun <<" are blue\n";
+	cout<< "I love "<< celebrity << "\n";
+} 
